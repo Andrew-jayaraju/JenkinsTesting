30941cab20c4b072524d25c2774557258f6dca2f
Andrew <Andrew.jayaraju@gmal.com>
2024-12-03 14:40:55 +0530

    Added SOUT

--- StudentClass.java
+++ StudentClass.java
@@ -2,6 +2,7 @@ public class StudentClass {
     public static void main(String[] args) {
         int arr[] = {10, 20, 30, 40, 50};
         int n = arr.length;
+        System.out.println("This is project of Sum of all the elements of an Array");
         int sum = 0;
         for(int i : arr){
             sum += i;
